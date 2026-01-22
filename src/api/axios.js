5f59910084efc02c66d82efea7bb20ac7be3683b
Brayan Padilla <padillasilesbrayandavid@gmail.com>
2026-01-22 18:12:15 -0400

    rutas

--- src/api/axios.js
+++ src/api/axios.js
@@ -5,8 +5,7 @@ const api = axios.create({
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
-    },
-    withCredentials: true 
+    }
 });
 
 api.interceptors.request.use(config => {
